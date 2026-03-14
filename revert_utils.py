1d9aa69df719d38898133f0dd0127e971bc38450
俞博弈 <164716248+yeyouyby@users.noreply.github.com>
2026-03-14 17:51:50 +0800

    Delete revert_utils.py

--- revert_utils.py
+++
@@ -1,6 +0,0 @@
-with open("utils.py", "r", encoding="utf-8") as f:
-    text = f.read()
-
-# Make sure only one copy of utils.py content is there, remove duplication if present.
-if text.count("import logging") > 2:
-    pass # Wait, let's just make sure it looks fine.
